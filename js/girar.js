dc2c90ae39d3ae314e0380f3aa749e8b6d656a2b
ezebran <ezebran@gmail.com>
2022-05-16 18:58:22 -0300

    more giros

--- js/girar.js
+++ js/girar.js
@@ -1,17 +1,23 @@
-let one = {  x: 0, y: 180, z: 90 };
+let one = {  x: 360, y: 180, z: 180 };
 
 let two = {  x: 90, y: 0, z: 270 };
 
-let trhee = {  x: 0, y: 90, z: 90 };
+let trhee = {  x: 360, y: 810, z: 810 };
 
-let four = {  x: 90, y: 0, z: 180 };
+let four = {  x: 270, y: 180, z: 360 };
 
-let five = {  x: 90, y: 0, z: 90 };
+let five = {  x: 270, y: 90, z: 270 };
 
 let six = {  x: 0, y: 0, z: 90 };
 
 $('#girar-btn').click(function(){
+    
     let diceNumber = parseInt(Math.random() * (7 - 1) + 1);
+    let aux = diceNumber;
+
+    console.log(aux)
+    console.log(diceNumber)
+
     let diceParams = diceNumber == 1 ? { ...one } :
                     diceNumber == 2 ? { ...two} :
                     diceNumber == 3 ? { ...trhee} :
@@ -23,6 +29,5 @@ $('#girar-btn').click(function(){
 
 
     $('.dice').css({"transform":`rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z}deg)`});
-    console.log(diceNumber)
-    console.log(diceParams)
+
 })
